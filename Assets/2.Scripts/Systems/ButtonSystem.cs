ad28bda2185078d8fe78669a106d2ced0b1ab75e
yu <91445334+yufooo@users.noreply.github.com>
2026-04-08 19:35:06 +0900

    Feat: Add ButtonSystem
    a/b/c 버튼 3개로 icon선택하는 ui 추가, dotween추가

---
+++ Assets/2.Scripts/Systems/ButtonSystem.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonSystem : MonoBehaviour
+{
+    [SerializeField] private Image[] iconImages;
+    private int currentIndex;
+
+    private void Start()
+    {
+        currentIndex = -1;
+        for (int i = 0; i < iconImages.Length; i++)
+        {
+            iconImages[i].DOFade(0.5f,0f);
+        }
+    }
+
+    public void ClickA()//선택
+    {
+        int prevIndex = currentIndex;
+        
+        currentIndex++;
+        if(currentIndex >= iconImages.Length)
+            currentIndex = 0;
+        
+        if(prevIndex >=0)
+            iconImages[prevIndex].DOFade(0.5f,0f);
+           
+        iconImages[currentIndex].DOFade(1f, 0f);
+        
+    }
+
+    public void ClickB()//확인
+    {
+        
+    }
+
+    public void ClickC()//취소
+    {
+        currentIndex = -1;
+        for (int i = 0; i < iconImages.Length; i++)
+        {
+            iconImages[i].DOFade(0.5f,0f);
+        }
+    }
+
+    
+}
